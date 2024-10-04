16e0c025474fcb0967f77513a92d807e1bb0ae0c
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-10-04 18:55:00 +1000

    fix: need to place analytics inside body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   );
 }
